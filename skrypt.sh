4d29cd38fa15d777ed1dab85f60a136cd3135d16
MateuszKxX <kaseja.mateusz05@gmail.com>
2025-06-18 16:33:46 +0200

    Add --help flag

--- skrypt.sh
+++ skrypt.sh
@@ -9,3 +9,9 @@ elif [[ $1 == "--logs" ]]; then
 		echo -e "$filename\nskrypt.sh\n$(date)" > $filename
 	done
 fi
+
+elif [[ $1 == "--help" ]]; then
+	echo "--date	Show current date"
+	echo "--logs [n]	Create n log files"
+	echo "--help	Show help"
+fi
