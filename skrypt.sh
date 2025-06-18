47bc1a769e760aa427fd5a8e2b0db1100ca6a8ca
MateuszKxX <kaseja.mateusz05@gmail.com>
2025-06-18 16:51:28 +0200

    Add error

--- skrypt.sh
+++ skrypt.sh
@@ -15,3 +15,11 @@ elif [[ $1 == "--help" ]]; then
 	echo "--logs [n]	Create n log files"
 	echo "--help	Show help"
 fi
+
+elif [[ $1 == "--error" || $1 == "-e" ]]; then
+	count=${2:-100}
+	for i in $(seq 1 $count); do
+		mkdir -p "error${i}"
+		echo -e "error${i}.txt\nskrypt.sh\n$(date)" > "error${i}/error${i}.txt"
+		done
+fi
