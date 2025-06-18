06177e11027c0913a885c019eefd9c4cdf87b326
MateuszKxX <kaseja.mateusz05@gmail.com>
2025-06-18 16:21:46 +0200

    Add --logs option to generate log files

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,11 @@
 if [[ &1 == "--date" ]]; then
 	date
 fi
+
+elif [[ $1 == "--logs" ]]; then
+	count=${2:-100}
+	for i in $(seq 1 $count); do
+		filename="log${i}.txt"
+		echo -e "$filename\nskrypt.sh\n$(date)" > $filename
+	done
+fi
